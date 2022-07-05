9a4952c202ce24a131f73be21f3331dc40dac789
Tullyo Ryann <tullyo.silva@sensedia.com>
2022-07-05 09:18:17 -0300

    remove code smell

--- wallet/src/main/java/com/tullyo/wallet/WalletApplication.java
+++ wallet/src/main/java/com/tullyo/wallet/WalletApplication.java
@@ -2,10 +2,8 @@ package com.tullyo.wallet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@ComponentScan({"com.tullyo.wallet.domain.ports.services"})
 public class WalletApplication {
 
 	public static void main(String[] args) {
