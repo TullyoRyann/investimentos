377a787d04aeaa70ee535e386a1ca45c58fd6003
Tullyo Ryann <tullyo.silva@sensedia.com>
2022-07-05 09:18:34 -0300

    Adiciona criação da tabela carteira

--- wallet/src/main/java/com/tullyo/wallet/infrastructure/configuration/DynamoDBConfig.java
+++ wallet/src/main/java/com/tullyo/wallet/infrastructure/configuration/DynamoDBConfig.java
@@ -4,6 +4,11 @@ import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.amazonaws.services.dynamodbv2.util.TableUtils;
+import com.tullyo.wallet.infrastructure.adapters.entities.WalletEntity;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -37,6 +42,16 @@ public class DynamoDBConfig {
   @Bean
   public AWSCredentials amazonAWSCredentials() {
     return new BasicAWSCredentials(amazonAWSAccessKey, amazonAWSSecretKey);
+  }
+
+
+  @Bean
+  public void createTableWallet() {
+    AmazonDynamoDB amazonDynamoDB = amazonDynamoDB();
+    DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(amazonDynamoDB);
+    CreateTableRequest tableRequest = dynamoDBMapper.generateCreateTableRequest(WalletEntity.class);
+    tableRequest.setProvisionedThroughput(new ProvisionedThroughput(1L, 1L));
+    TableUtils.createTableIfNotExists(amazonDynamoDB, tableRequest);
   }
 
 }
