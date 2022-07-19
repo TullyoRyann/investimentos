c384e6282ddd9a3b38e3a681ebdee3530a69a4b2
Tullyo Ryann <tullyo.silva@sensedia.com>
2022-07-19 10:05:16 -0300

    Adiciona ignorar campos nulos

--- wallet/src/main/java/com/tullyo/wallet/infrastructure/adapters/inbound/dtos/response/ErrorResponse.java
+++ wallet/src/main/java/com/tullyo/wallet/infrastructure/adapters/inbound/dtos/response/ErrorResponse.java
@@ -1,5 +1,7 @@
 package com.tullyo.wallet.infrastructure.adapters.inbound.dtos.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,6 +9,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Builder
+@JsonInclude(Include.NON_NULL)
 public class ErrorResponse {
 
   private String code;
